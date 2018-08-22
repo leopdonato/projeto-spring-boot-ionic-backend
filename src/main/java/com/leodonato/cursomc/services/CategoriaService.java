e92266a2276a86be5ec3fcdbd6d8d594d0bd30da
Leonardo Donato <leo_donato007@yahoo.com.br>
2018-08-22 20:21:23 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/leodonato/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.leodonato.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leodonato.cursomc.domain.Categoria;
+import com.leodonato.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		}
+
+}
