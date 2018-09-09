4c4741b8b3204429e19b2b28323803f6dc796c23
Leonardo Donato <leo_donato007@yahoo.com.br>
2018-09-09 15:50:22 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/leodonato/cursomc/services/CategoriaService.java
+++ src/main/java/com/leodonato/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.leodonato.cursomc.domain.Categoria;
+import com.leodonato.cursomc.dto.CategoriaDTO;
 import com.leodonato.cursomc.repositories.CategoriaRepository;
 import com.leodonato.cursomc.services.exceptions.DataIntegrityException;
 import com.leodonato.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
