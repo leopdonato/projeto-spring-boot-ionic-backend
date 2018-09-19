6e5223e85841e8357c5edb9e7aa29cd961718af2
Leonardo Donato <leo_donato007@yahoo.com.br>
2018-09-19 20:32:59 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/leodonato/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/leodonato/cursomc/repositories/ProdutoRepository.java
@@ -2,12 +2,25 @@ package com.leodonato.cursomc.repositories;
 
 
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.leodonato.cursomc.domain.Categoria;
 import com.leodonato.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
+	
+	@Transactional(readOnly=true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+					//substituindo o "search", pode-se retirar a anotação Query e Param, resultado é o mesmo
 }
