fa031159fe76bff366cbd59e8553dee2d7831d3b
Leonardo Donato <leo_donato007@yahoo.com.br>
2018-10-14 22:22:33 -0300

    Configuracao e envio do projeto para producao

--- src/main/java/com/leodonato/cursomc/resources/PedidoResource.java
+++ src/main/java/com/leodonato/cursomc/resources/PedidoResource.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.leodonato.cursomc.domain.Categoria;
 import com.leodonato.cursomc.domain.Pedido;
-import com.leodonato.cursomc.dto.CategoriaDTO;
 import com.leodonato.cursomc.services.PedidoService;
 
 @RestController
