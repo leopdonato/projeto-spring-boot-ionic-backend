30476ee92008a355761c95e84870b02a39363204
Leonardo Donato <leo_donato007@yahoo.com.br>
2018-10-16 22:00:53 -0300

    Email HTML

--- src/main/java/com/leodonato/cursomc/services/EmailService.java
+++ src/main/java/com/leodonato/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.leodonato.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.leodonato.cursomc.domain.Pedido;
@@ -10,4 +12,7 @@ public interface EmailService {
 		
 	void sendEmail(SimpleMailMessage msg);
 
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
