98d1a4e9f58bf9a9cf767e2ee21622386d7135c5
Leonardo Donato <leo_donato007@yahoo.com.br>
2018-11-06 17:38:18 -0200

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/leodonato/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/leodonato/cursomc/repositories/PedidoRepository.java
@@ -2,12 +2,18 @@ package com.leodonato.cursomc.repositories;
 
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.leodonato.cursomc.domain.Cliente;
 import com.leodonato.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
-
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
--- src/main/java/com/leodonato/cursomc/services/PedidoService.java
+++ src/main/java/com/leodonato/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.leodonato.cursomc.domain.Cliente;
 import com.leodonato.cursomc.domain.ItemPedido;
 import com.leodonato.cursomc.domain.PagamentoComBoleto;
 import com.leodonato.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.leodonato.cursomc.domain.enums.EstadoPagamento;
 import com.leodonato.cursomc.repositories.ItemPedidoRepository;
 import com.leodonato.cursomc.repositories.PagamentoRepository;
 import com.leodonato.cursomc.repositories.PedidoRepository;
+import com.leodonato.cursomc.security.UserSS;
+import com.leodonato.cursomc.services.exceptions.AuthorizationException;
 import com.leodonato.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -72,6 +78,16 @@ public class PedidoService {
 		//emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso Negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
